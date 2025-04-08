67d6bb215c41aa9e73631c4cc7866e182b068429
Christoph Van Jaarsveld <christophvj08@gmail.com>
2025-04-08 19:50:40 +0200

    Add project files.

---
+++ GigLink/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace GigLink.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ GigLink/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GigLink.WebApiApplication" Language="C#" %>
