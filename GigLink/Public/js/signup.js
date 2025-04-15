cc006141a0f592429afdaedf5406b67f879fcb11
LuantheByteBender <luanduplessis0401@gmail.com>
2025-04-15 20:19:36 +0200

    Edited the welcome page

--- GigLink/Public/js/signup.js
+++ GigLink/Public/js/signup.js
@@ -2,10 +2,8 @@ const classificationInput = document.getElementById('classificationInput');
 const nameInput = document.getElementById('nameInput');
 const nameLabel = document.getElementById('nameLabel');
 
-
 const passwordInput = document.getElementById('passwordInput');
 const passwordConfirmInput = document.getElementById('passwordConfirmInput');
-
 
 //! Add on change event listener to the classification input
 classificationInput.addEventListener('change', () => {
@@ -14,23 +12,19 @@ classificationInput.addEventListener('change', () => {
     if (classificationValue == 'Band') {
         nameLabel.innerText = 'Band Name';
         nameInput.placeholder = 'Enter band name';
-    }else if (classificationValue == 'Artist') {
+    } else if (classificationValue == 'Artist') {
         nameLabel.innerText = 'Artist Name';
         nameInput.placeholder = 'Enter artist name';
-    }else {
+    } else {
         nameLabel.innerText = 'Venue Name';
         nameInput.placeholder = 'Enter Venue name';
     }
-
 });
-
 
 //! Add on change event listener to the password input and confirm password input
 
-
-
 const validatePasswords = () => {
-    if (passwordInput.value.length < 8 ) {
+    if (passwordInput.value.length < 8) {
         alert('Password must be at least 8 characters long');
         passwordConfirmInput.value = '';
         passwordInput.value = '';
